33c66b17814c083cc2b96e99ee2de6f28fabb8fc
GD.Nguyen <gdnguyen@users.noreply.github.com>
2020-02-26 20:59:11 +0700

    cm1

---
+++ A11/src/main/java/com/se00502x/lab321x/A11Application.java
@@ -0,0 +1,13 @@
+package com.se00502x.lab321x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A11Application.class, args);
+	}
+
+}
---
+++ A11/src/test/java/com/se00502x/lab321x/A11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se00502x.lab321x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
